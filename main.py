fffdd5d9244be28d6a4f0a607e3127bdff70c570
Bhargav Das Gupta <dasguptabhargav@gmail.com>
2022-12-24 23:02:58 +0530

    update main.py
    import download_playlist function from download.py

--- main.py
+++ main.py
@@ -1,17 +1,7 @@
 #!/usr/bin/env python3
 
-from pytube import YouTube
+from download import download_playlist
 
-def download(link):
-   ytObj = YouTube(link)
-   ytObj = ytObj.streams.get_highest_resolution()
-
-   try:
-      ytObj.download()
-   except:
-      print("An error occurred")
-
-   print("Download completed successfully")
-
-link = input("Enter youtube video url: ")
-download(link)
+if __name__ == '__main__':
+   link = input("Enter youtube video url: ")
+   download_playlist(link)
