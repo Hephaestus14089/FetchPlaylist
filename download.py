1cd678fc66949e42311aedaee7b76d3d7aa5898d
Bhargav Das Gupta <dasguptabhargav@gmail.com>
2022-12-26 01:02:47 +0530

    update download.py
    accept link as argument, check if playlist is found

--- download.py
+++ download.py
@@ -4,22 +4,26 @@ def get_playlist(link):
    return contrib.playlist.Playlist(link)
 
 def get_videos(playlistObj):
-    return playlistObj.videos
+   return playlistObj.videos
 
 def download_video(videoObj):
-    videoObj.streams.get_highest_resolution()
-    try:
-        videoObj.download()
-        print("Done.")
-    except:
-        print("Unknown error while downloading video")
+   videoObj.streams.get_highest_resolution()
+   try:
+      videoObj.download()
+      print("Done.")
+   except:
+      print("Unknown error while downloading video")
 
-def download_playlist():
-    playlistObj = get_playlist(link)
-    videoObjList = get_videos(playlistObj)
+def download_playlist(link):
+   playlistObj = get_playlist(link)
+   try:
+      playlistTitle = playlistObj.title
+      print(f'Playlist found: "{playlistTitle}"')
+   except:
+      print("Error: Unable to find playlist\t:(")
+      return None
 
-    print("Videos in playlist are: ")
-    for i in range(3):
-        videoObj = videoObjList[i]
-        print(f"Downloading {videoObj.title} ...")
-        download_video(videoObj)
+   videoObjList = get_videos(playlistObj)
+   for videoObj in videoObjList:
+      print(f'Downloading "{videoObj.title}"...')
+      download_video(videoObj)
