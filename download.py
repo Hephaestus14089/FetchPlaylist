76a17bbdc7d954bf63651eaa21c7d4a8d78839b0
Bhargav Das Gupta <dasguptabhargav@gmail.com>
2022-12-27 18:08:22 +0530

    implement progress bar, major bug fix
    download progress bar added, major bug fixed: stream object used for downloading

--- download.py
+++ download.py
@@ -1,15 +1,21 @@
 from pytube import contrib
+from pytube import YouTube
+from pytube.cli import on_progress
 
 def get_playlist(link):
    return contrib.playlist.Playlist(link)
 
-def get_videos(playlistObj):
-   return playlistObj.videos
+def get_videoObj_list(playlistObj):
+   videoUrlList = playlistObj.video_urls
+   videoObjList = []
+   for videoUrl in videoUrlList:
+      videoObjList.append(YouTube(videoUrl, on_progress_callback=on_progress))
+   return videoObjList
 
 def download_video(videoObj):
-   videoObj.streams.get_highest_resolution()
+   stream = videoObj.streams.get_highest_resolution()
    try:
-      videoObj.download()
+      stream.download()
       print("Done.")
    except:
       print("Unknown error while downloading video")
@@ -23,7 +29,7 @@ def download_playlist(link):
       print("Error: Unable to find playlist\t:(")
       return None
 
-   videoObjList = get_videos(playlistObj)
+   videoObjList = get_videoObj_list(playlistObj)
    for videoObj in videoObjList:
       print(f'Downloading "{videoObj.title}"...')
       download_video(videoObj)
